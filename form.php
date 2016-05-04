348a7cfe22613396bae027016650e0f27a661b2e
Yuriy-chechenets <meatwag@yandex.ru>
2016-05-05 03:58:29 +0400

    Закончил долбаный калькулятор - только строки не знаю, как выключить.

--- form.php
+++ form.php
@@ -1,27 +1,75 @@
 <?php
-
+// ini_set('display_errors', 0);
 /*
  Необходимо создать калькулятор. В него передается 2 значения, по результатам которых выводится ответ в зависимости от выбранной операции.
-Данные, введенный в форму, должный сохраняться.
+Данные, введенные в форму, должный сохраняться в соответствующих окнах.
  */
-
 $timeBegin = microtime(true);
-echo "Этот скрипт - учебный, созданный для работы с формами. В нем буду делать калькулятор.<br>";
+/*
+if (!empty($_POST['var_1']) && !empty($_POST['var_2']))
+{
+    // echo "Значение VAR_1 переданное в скрипт : ". $_POST['var_1']."<br>";
+    // echo "Значение VAR_2 переданное в скрипт : ". $_POST['var_2']."<br>";
+    echo var_dump($_POST['var_1'])."<br>";
+    echo var_dump($_POST['var_2'])."<br>";
+    $first = is_string($_POST['var_1']);
+    $second = is_string($_POST['var_2']);
+    if ($first == "true" ) {$c="Вы ввели не числовые данные.";}
+    if ($second == "true") {$c="Вы ввели не числовые данные.";}
+    echo var_dump($first)."<br>";
+    echo $first."<br>";
+    echo $second."<br>";
 
+}
+*/
+if ( isset($_POST['var_1']) && isset($_POST['var_2']) && isset($_POST['operat']) && is_numeric($_POST['var_1']) && is_numeric($_POST['var_2']) )
+{
+    echo "Значение VAR_1 переданное в скрипт : ". $_POST['var_1']."<br>";
+    echo "Значение VAR_2 переданное в скрипт : ". $_POST['var_2']."<br>";
+    echo "Значение оператора переданное в скрипт : ". $_POST['operat']."<br>";
+$operation = $_POST['operat'];
+    switch($operation)
+    {
+        case '+': $c = ( ($_POST['var_1']+0) + ($_POST['var_2']+0) ); break;
+        case '-': $c = ( ($_POST['var_1']+0) - ($_POST['var_2']+0) ); break;
+        case '*': $c = ( ($_POST['var_1']+0) * ($_POST['var_2']+0) ); break;
+        case '/':
+                 if($_POST['var_2']==0)
+                    {$c ='Деление на ноль недопустимо'; break;}
+                 else
+                     {$c = ($_POST['var_1']+0) / ($_POST['var_2']+0); break;}
+    }
+    /*
+    if ($_POST['operat'] == "sum") {$c = $_POST['var_1']*1 + $_POST['var_2']*1; }
+    if ($_POST['operat'] == "diff") {$c = $_POST['var_1']*1 - $_POST['var_2']*1; }
+    if ($_POST['operat'] == "mult") {$c = $_POST['var_1']*1 * $_POST['var_2']*1; }
+    if ($_POST['operat'] == "div") {$c = $_POST['var_1']*1 / $_POST['var_2']*1; }
+    */
+}
+
+else {$c='';}
+
+echo "Этот скрипт - учебный, созданный для работы с маленькими числовыми формами. <br>В нем буду делать калькулятор.<br>";
 
 ?>
+<!doctype=html>
 <html>
-<form action="form.php" method=POST>
+<form  method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?> " >
     <fieldset>
-        <legend>Калькулятор человечества</legend>
-        <input type="text" name="var_1" id="id_1" size=6 maxlength=15 placeholder="Число 1">
-        <select name="option" required id="option_id">
-              <option value="1">+</option>
-              <option value="2">-</option>
-              <option value="3">*</option>
-              <option value="4">/</option>
+        <legend>Лучший в мире калькулятор за всю историю человечества</legend>
+        <input type="text" name="var_1" maxlength="15" value="<?php if(isset($_POST['var_1'])) {echo $_POST['var_1'];} else echo "";?> "  />
+        <select name = "operat" >
+            <option name="1" <?php if (isset($_POST['operat']) &&  $_POST['operat']=='+') echo "selected"; ?> >+</option>
+            <option name="2" <?php if (isset($_POST['operat']) && $_POST['operat']=='-') echo "selected"; ?> >-</option>
+            <option name="3" <?php if (isset($_POST['operat']) && $_POST['operat']=='*') echo "selected"; ?> >*</option>
+            <option name="4" <?php if (isset($_POST['operat']) && $_POST['operat']=='/') echo "selected"; ?> >/</option>
         </select>
-        <input type="text" name="var_2" id="id_2" size=6 maxlength=15 placeholder="Число 2">
+        <input type="text" name="var_2" id="id_2" size=6 maxlength=15 value="<?php if(isset($_POST['var_2'])) {echo $_POST['var_2'];} else echo "";?>"/>
+        <input type="submit" value="=">
+        <?php
+        echo "Ответ: ".$c." </br>";
+        echo "скрипт выполнился за ". (microtime(true)-$timeBegin) . " секунд";
+        ?>
     </fieldset>
 </form>
 </html>
